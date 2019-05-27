33e3bf81c90df142b823f16c6fb813f64dbe2cdb
wandieinnocents <wandieinnocent2@gmail.com>
2019-05-27 16:34:59 +0300

    delete record from database

--- Main.py
+++ Main.py
@@ -20,8 +20,15 @@ def addRecords(Name,Age,Sex,Address):
     db.row_factory = sqlite3.Row
     # creating the table
     db.execute("insert into Admin(Name,Age,Sex,Address) values(?,?,?,?)", (Name, Age, Sex, Address))
-
+    print("Record has been added successfuly! ")
     # commit/add data to the database
+    db.commit()
+
+def deleteRecords(name):
+    db.row_factory = sqlite3.Row
+    # creating the table
+    db.execute("delete from Admin where Name = '{}'".format(name))
+    print("Record has been deleted successfully! ")
     db.commit()
 
 def listAdmins():
@@ -38,21 +45,35 @@ def listAdmins():
 
 def main():
 
-    #call create table function
-    createTable()
-    indexOperation = int(input("\n\tSELECT AN OPERATION \n\n\t 1 - Add Record / 2 List Admins -  "))
+    #loop the application to always run
+    while 1:
+        # call create table function
+        createTable()
+        indexOperation = int(input("\n\tSELECT AN OPERATION "
+                                   "\n\n\t 1.Add Record."
+                                   " \n\t 2.List Admins."
+                                   "\n\t 3.Delete Admin "
+                                   " \n\t 0.Exit."))
 
-    #capture selection of input
-    #call funciton addRecords if selction == 1
-    if(indexOperation == 1):
-        Name = input("\n\tEnter Name : ")
-        Age =  int(input("\n\tEnter age : "))
-        Sex = input("\n\tEnter Sex (Male/Female/None) : ")
-        Address = input("\n\tEnter Your Address : ")
-        addRecords(Name,Age,Sex,Address)
-    elif(indexOperation == 2):
-        #call function listAdmins if selection == 2
-        listAdmins()
+
+        if(indexOperation == 0):
+            break;
+        # capture selection of input
+        # call funciton addRecords if selction == 1
+        if (indexOperation == 1):
+            Name = input("\n\tEnter Name : ")
+            Age = int(input("\n\tEnter age : "))
+            Sex = input("\n\tEnter Sex (Male/Female/None) : ")
+            Address = input("\n\tEnter Your Address : ")
+            addRecords(Name, Age, Sex, Address)
+        elif (indexOperation == 2):
+            # call function listAdmins if selection == 2
+            listAdmins()
+        elif (indexOperation == 3):
+            print("Delete User from Database: ")
+            Name = input("Enter the user u need to delete: ")
+            deleteRecords(Name)
+
 
 
 
